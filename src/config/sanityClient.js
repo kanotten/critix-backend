43497fa903ce737080a11531ad5eb43bd9662085
AbdullaAlHarun <abdhar01276@stud.noroff.no>
2025-03-27 23:01:14 +0100

    fixs: bugs

--- src/config/sanityClient.js
+++ src/config/sanityClient.js
@@ -1,12 +1,11 @@
-import sanityClient from "../config/sanityClient.js"; 
-import dotenv from "dotenv";
+import sanityClient from '@sanity/client';
 
-dotenv.config();
-
-export default sanityClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET || "production",
-  useCdn: false, 
-  apiVersion: "2023-03-25",
-  token: process.env.SANITY_WRITE_TOKEN, 
+const client = sanityClient({
+  projectId: process.env.SANITY_PROJECT_ID || '',
+  dataset: process.env.SANITY_DATASET || 'production',
+  useCdn: false,
+  apiVersion: '2023-03-25',
+  token: process.env.SANITY_WRITE_TOKEN || '',
 });
+
+export default client;
